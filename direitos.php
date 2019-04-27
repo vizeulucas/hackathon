de59bc93c1beb2b49991a75ee9f03ab140f1a908
Guilherme Cabral <gbral.js@gmail.com>
2019-04-27 10:25:25 -0300

    Add files via upload

---
+++ direitos.php
@@ -0,0 +1,20 @@
+<?php
+    include('conexao.php');
+    include('cabecalho-logado.php');
+ ?>
+  
+		<h1>Direitos do Ex-Detento</h1>
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer id metus mauris. Aenean pellentesque ligula pharetra ligula fermentum, et tempus ex convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec aliquam, urna eu ultrices molestie, tortor arcu iaculis nisi, eget volutpat ante ante eget justo. Curabitur ut lorem non risus vehicula pharetra ac a nunc. Etiam quis scelerisque ligula. Maecenas eget cursus magna. Vivamus interdum, eros auctor facilisis molestie, eros eros tristique dui, vel tincidunt sapien nunc sit amet diam. Quisque ut tincidunt odio.
+
+				Donec vel enim ex. Aenean pellentesque nisi placerat mauris tempor, sed lobortis leo consectetur. Sed malesuada porta libero id porta. Nullam ultrices, orci a facilisis maximus, mauris sapien laoreet magna, nec rhoncus lorem libero ut lectus. Vivamus viverra massa metus. Nulla vulputate a ex ut feugiat. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque ultrices odio at libero ornare sagittis. Donec a elementum diam. Aenean vulputate magna sollicitudin ipsum ullamcorper tristique. Integer vestibulum lacus sed ultrices auctor. Suspendisse nibh diam, ultricies ut volutpat ac, dignissim ac eros. Nam mollis, mi id tincidunt dictum, nulla quam egestas magna, eu imperdiet nisi orci sit amet leo. Proin eleifend, augue non ultrices elementum, ante nisi rutrum magna, nec faucibus lorem arcu lobortis massa.
+
+				Quisque tristique neque tempus tortor interdum commodo. Duis viverra cursus ante nec faucibus. Suspendisse sed ornare ligula. Quisque at aliquam nibh. Etiam finibus, neque nec auctor sollicitudin, massa sem imperdiet tortor, vel dictum felis lacus vitae erat. Maecenas eu convallis dui, ac aliquet ante. Phasellus ornare diam vitae mauris elementum ultrices. Morbi semper nunc at dictum finibus.
+
+				Donec faucibus dui quis ipsum volutpat tempor. Donec pulvinar est vel orci ullamcorper, et ullamcorper est condimentum. Praesent congue dui quis aliquet congue. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer dapibus odio ullamcorper enim sagittis, at vulputate tellus aliquet. In ut cursus dolor. Nulla mattis semper ex, id mollis risus pellentesque et. Nulla ac arcu auctor, porta ipsum vitae, tristique quam.
+
+				In scelerisque ex at enim posuere, et porta est hendrerit. Donec gravida turpis a elit ornare, non laoreet metus imperdiet. Aenean pulvinar cursus ipsum. Nam vulputate enim urna, sed egestas justo elementum et. Curabitur aliquet fermentum molestie. Nunc vehicula ullamcorper massa. Nullam at turpis nisl. Vivamus erat quam, congue ut viverra ac, auctor non ex. Nullam ac magna augue.
+
+				Generated 5 paragraphs, 400 words, 2718 bytes of Lorem Ipsum</p>
+	</body>
+</html>				
