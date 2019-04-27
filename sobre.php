1a6445e2f4ea2d3dfdbb12c70d1cff4345accf4c
Guilherme Cabral <gbral.js@gmail.com>
2019-04-27 09:32:35 -0300

    Add files via upload

---
+++ sobre.php
@@ -0,0 +1,29 @@
+﻿<?php 
+    include("cabecalho.php");
+?>
+		<br>
+		<br>
+		<h1 style="text-align: center;">A Iniciativa Restarter</h1>
+		<br>
+
+		<p style="margin-left: 20px; margin-right: 20px; text-align: justify;">
+			O objetivo principal da nossa proposta é reduzir os altos índices de reincidência de presidiários e ex-presidiarios em atos criminosos, trazendo o cidadão novamente para os parâmetros da sociedade, ou seja, a ressocialização.
+
+			De acordo com o site do governo do Brasil, empresas com contrato de serviço por licitação com o Poder Executivo, em valores acima de R$ 330 mil, são obrigadas a reservar vagas para presos ou egressos do sistema prisional. A empresa terá de reservar de 3% a 6% da sua mão de obra. O percentual irá variar conforme a quantidade de pessoas necessárias para prestar o serviço. (limpeza, vigilância, alimentação, etc...) Está incluso, presos provisórios, presos em regime fechado, semiaberto e aberto, além daqueles que já cumpriram a pena e estão contemplados pelo decreto. Os detentos devem ser autorizados pelo juiz de Execução Penal, ter cumprido ao menos um sexto da pena e cumprir requerimentos como aptidão e disciplina. Porém, nem todos os presos estão aptos ao trabalho.
+
+			De acordo com o presidente do supremo tribunal federal, Cezar Peluso, menos de 20% dos 500 mil presos existentes no Brasil trabalham, e menos de 8% estudam. Podemos ver por aí que temos um desafio enorme pela frente no sentido de qualificar esta população e quebrar este ciclo de criminalidade que vem sendo gerado ao longo do tempo.
+
+			A maior dificuldade para inserir os presos no mercado de trabalho é a falta de qualificação e estudo. Por esse motivo, a primeira barreira para o preso é justamente conseguir um tipo de qualificação que seja compatível com seu grau de instrução.
+
+ 			Diante disso, criamos uma aplicação web onde o usuário irá se cadastrar e ter acesso a um questionário para identificar seus conhecimentos prévios, mediante aos resultados será filtrada determinadas vagas de empregos que se enquadram ao perfil dele, as vagas são retiradas do diário oficial. Além disso, também será disponibilizado diversos meios para que eles possam aprimorar seus conhecimentos.  Como alguns programas e plataformas de ensino, para ele poder se capacitar até que seja possível ele ter alguma forma de emprego, de sustento e de renda.
+
+			Bases de dados= Iremos nos comprometer em alimentar a base de dados uma vez por semana.
+
+ 
+
+			Sete em cada dez ex-presidiários voltam ao crime. 
+		</p>
+
+			<center><a button href="http://localhost/hackathon/cadastro.php" type="button" class="btn btn-danger">Cadastro</button></a></center>
+	</body>
+</html>
